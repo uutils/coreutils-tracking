edddd7683d0d8b03abf66530f3048f1aa0564d85
Sylvestre Ledru <sylvestre@debian.org>
2021-05-20 15:11:06 +0200

    Improve the graphic

--- graph.py
+++ graph.py
@@ -1,16 +1,32 @@
+from datetime import datetime
+from email.utils import parsedate
+
+import sys
+import time
+
 import matplotlib.pyplot as plt
 import pandas as pd
-import sys
 
-d = pd.read_json(sys.argv[1])
+d = pd.read_json(sys.argv[1], orient="index")
 df = pd.DataFrame(d)
-df = df.transpose()
+
+df.columns.names = ["date"]
+
+as_list = df.index.tolist()
+for i in as_list:
+    idx = as_list.index(i)
+    t = parsedate(i)
+    as_list[idx] = datetime.fromtimestamp(time.mktime(t))
+
+df.index = as_list
+
 print(df)
 
 ax = plt.gca()
 df.plot(y="total", color="blue", ax=ax)
 df.plot(y="fail", color="red", ax=ax)
 df.plot(y="pass", color="green", ax=ax)
-
+df.plot(y="error", color="orange", ax=ax)
+plt.title("Rust/Coreutils running GNU's testsuite")
 plt.xticks(rotation=45)
 plt.savefig("gnu-results.png", dpi=199)
